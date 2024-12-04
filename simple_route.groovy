db6a519af25e147ecebf655cd0cac9f8ca78fd42
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-12-04 09:48:22 -0600

    Primeros pasos con Camel

---
+++ simple_route.groovy
@@ -0,0 +1,10 @@
+
+@Grapes(
+  @Grab(group='org.apache.camel', module='camel-core', version='4.8.1')
+)
+
+import org.apache.camel.CamelContext
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.builder.RouteBuilder
+
+println "Hola mundo"
