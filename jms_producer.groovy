1472e09567ab62c5bd661ef1fb1a5a6607ed835a
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-12-04 11:57:03 -0600

    Produciendo mensajes para ActiveMQ

--- jms_producer.groovy
+++ jms_producer.groovy
@@ -26,14 +26,32 @@ producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT)
 
 // Enviar mensajes a la cola
 println "Enviando mensajes a la cola JMS '${queueName}'..."
-(1..5).each { i ->
-  String messageContent = "Order ${i} - Priority: ${(i % 2 == 0) ? 'High' : 'Low'}"
-  TextMessage message = session.createTextMessage(messageContent)
-  message.setStringProperty("type", (i % 2 == 0) ? "electronics" : "clothing")
-  message.setStringProperty("priority", (i % 2 == 0) ? "high" : "low")
+
+def orders = [
+  [id: 1, type: 'electronic', group: 'A'],
+  [id: 2, type: 'clothes', group: 'B'],
+  [id: 3, type: 'electronic', group: 'A'],
+  [id: 4, type: 'clothes', group: 'B'],
+  [id: 5, type: 'electronic', group: 'A'],
+  [id: 6, type: 'clothes', group: 'B']
+]
+
+orders.each { order ->
+  TextMessage message = session.createTextMessage(order.toString())
+  message.setStringProperty("type", order.type)
+  message.setStringProperty("group", order.group)
   producer.send(message)
-  println "Mensaje enviado: ${messageContent}"
+  println "Mensaje enviado: ${order}"
 }
+
+// (1..5).each { i ->
+//   String messageContent = "Order ${i} - Priority: ${(i % 2 == 0) ? 'High' : 'Low'}"
+//   TextMessage message = session.createTextMessage(messageContent)
+//   message.setStringProperty("type", (i % 2 == 0) ? "electronics" : "clothing")
+//   message.setStringProperty("priority", (i % 2 == 0) ? "high" : "low")
+//   producer.send(message)
+//   println "Mensaje enviado: ${messageContent}"
+// }
 
 // Cerrar recursos
 producer.close()
